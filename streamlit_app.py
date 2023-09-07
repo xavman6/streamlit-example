2f7362af0ef1f2c110ad62b4de11378883127705
xavman6 <90087639+xavman6@users.noreply.github.com>
2023-09-07 21:40:36 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import time
 
-"""
-# Welcome to Streamlit!
+# Set up the page's title and header
+st.title("Timer App")
+st.header("Start and Stop A Timer")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Initialize the time
+time_elapsed = 0
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Create the UI
+start_stop = st.button("Start/Stop Timer")
+reset = st.button("Reset Timer")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Define the actions
+if start_stop:
+    if start_stop.text == "Start Timer":
+        start_stop.text = "Stop Timer"
+        while start_stop.text == "Stop Timer":
+            time_elapsed += 1
+            st.write(f"Time Elapsed: {time_elapsed} seconds")
+            time.sleep(1)
+    else:
+        start_stop.text = "Start Timer"
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Reset the timer
+if reset:
+    time_elapsed = 0
+    st.write("Timer reset")
