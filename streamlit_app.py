41f247074ff7fd2855ded1b1e09b61ad4f4b7a76
xavman6 <90087639+xavman6@users.noreply.github.com>
2023-09-07 22:37:23 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,26 @@
 # Create the Streamlit App
 
 import streamlit as st 
+import time
 
 # Set the title and header 
 st.title("Timer App")
 st.header("Start and Stop Timer")
 
+# Placeholder for timer
+timer_placeholder = st.empty()
+
 # Create the Start/Stop toggle
 start_stop = st.button("Start Timer")
 
 # Create the timer 
-if start_stop == "Start Timer":
+if start_stop:
     timer = 0 
-    while start_stop != "Stop Timer":
+    while True:
         timer += 1
-        st.write(timer)
-    else:
-        st.write("Timer stopped")
-        st.button("Start Timer")
+        timer_placeholder.write(timer)
+        time.sleep(1)
+        if st.button("Stop Timer"):
+            timer_placeholder.write("Timer stopped")
+            break
+
