bde024ebf4610a449b31cb29bb457731914238a3
xavman6 <90087639+xavman6@users.noreply.github.com>
2023-09-07 22:14:17 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,20 @@
-import streamlit as st
-import time
+# Create the Streamlit App
 
-# Set up the page's title and header
+import streamlit as st 
+
+# Set the title and header 
 st.title("Timer App")
-st.header("Start and Stop A Timer")
+st.header("Start and Stop Timer")
 
-# Initialize the time
-time_elapsed = 0
+# Create the Start/Stop toggle
+start_stop = st.button("Start Timer")
 
-# Create the UI
-start_stop = st.button("Start/Stop Timer")
-reset = st.button("Reset Timer")
-
-# Define the actions
-if start_stop:
-    if start_stop.text == "Start Timer":
-        start_stop.text = "Stop Timer"
-        while start_stop.text == "Stop Timer":
-            time_elapsed += 1
-            st.write(f"Time Elapsed: {time_elapsed} seconds")
-            time.sleep(1)
+# Create the timer 
+if start_stop.text == "Start Timer":
+    timer = 0 
+    while start_stop.text != "Stop Timer":
+        timer += 1
+        st.write(timer)
     else:
-        start_stop.text = "Start Timer"
-
-# Reset the timer
-if reset:
-    time_elapsed = 0
-    st.write("Timer reset")
+        st.write("Timer stopped")
+        st.button("Start Timer")
